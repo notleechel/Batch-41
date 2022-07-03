cb43e285afa0231f3ebc6776df95aa0ea4138527
Leechel Ella Recalde <ella.recalde2@gmail.com>
2022-07-03 16:20:43 +0800

    Import code files to git

---
+++ Frontend/src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello Batch 41</h1>
+        <input type="text" placeholder="what needs to be done?">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        
+    </div>
+
+</template>
